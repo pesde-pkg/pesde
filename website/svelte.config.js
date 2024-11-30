70f3bec275a6f834d98bb2755aeee089fed6f376
Luka <47296785+lukadev-0@users.noreply.github.com>
2024-11-30 12:42:41 +0100

    feat: use cloudflare adapter for main site (#10)

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,6 +1,6 @@
-import { mdsvex } from "mdsvex"
+import adapter from "@sveltejs/adapter-cloudflare"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-import adapter from "@sveltejs/adapter-vercel"
+import { mdsvex } from "mdsvex"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
