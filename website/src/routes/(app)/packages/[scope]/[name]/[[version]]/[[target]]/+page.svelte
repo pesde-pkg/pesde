0dde647042b66e1c805c67b0d6d43815fb74ba32
daimond113 <72147841+daimond113@users.noreply.github.com>
2024-12-15 12:37:24 +0100

    fix(website): render imgs inline

--- website/src/routes/(app)/packages/[scope]/[name]/[[version]]/[[target]]/+page.svelte
+++ website/src/routes/(app)/packages/[scope]/[name]/[[version]]/[[target]]/+page.svelte
@@ -2,7 +2,9 @@
 	const { data } = $props()
 </script>
 
-<div class="prose min-w-0 py-8 prose-pre:w-full prose-pre:overflow-auto">
+<div
+	class="prose prose-pre:w-full prose-pre:overflow-auto prose-img:inline-block prose-img:m-0 prose-video:inline-block prose-video:m-0 min-w-0 py-8"
+>
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	{@html data.readmeHtml}
 </div>
