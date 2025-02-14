509838bb08e849d4064d8f9f474d7435c490cc71
daimond113 <contact@daimond113.com>
2025-02-15 00:03:05 +0100

    fix: remove typo in duplicate key error
    The message previously contained "at line", but
    we don't have a way of knowing the line number so
    there was nothing after that. This commit removes
    the "at line" part of the message.

--- src/util.rs
+++ src/util.rs
@@ -172,7 +172,7 @@ where
 			while let Some((key, value)) = access.next_entry()? {
 				if map.contains_key(&key) {
 					return Err(serde::de::Error::custom(format!(
-						"duplicate key `{key}` at line"
+						"duplicate key `{key}`"
 					)));
 				}
 
