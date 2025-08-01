9d4a71e84b0636766552c0c0936263b1f1b746dd
daimond113 <contact@daimond113.com>
2025-08-01 17:09:56 +0200

    feat(registry): sort search results by relevance before publish date

--- registry/CHANGELOG.md
+++ registry/CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Changed
+- Sort search results by relevance, then by publish date by @daimond113
+
 ## [0.2.3-rc.3] - 2025-06-07
 ### Changed
 - Update pesde library by @daimond113
--- registry/src/endpoints/search.rs
+++ registry/src/endpoints/search.rs
@@ -26,27 +26,64 @@ pub async fn search_packages(
 	let searcher = app_state.search_reader.searcher();
 	let schema = searcher.schema();
 
-	let id = schema.get_field("id").unwrap();
+	let id_field = schema.get_field("id").unwrap();
 
 	let query = request_query.query.trim();
 
-	let query = if query.is_empty() {
-		Box::new(AllQuery)
-	} else {
-		app_state.query_parser.parse_query(query)?
-	};
+	let (count, top_docs) = if query.is_empty() {
+		let (count, top_docs) = searcher
+			.search(
+				&AllQuery,
+				&(
+					Count,
+					tantivy::collector::TopDocs::with_limit(50)
+						.and_offset(request_query.offset)
+						.order_by_fast_field::<DateTime>("published_at", Order::Desc),
+				),
+			)
+			.unwrap();
 
-	let (count, top_docs) = searcher
-		.search(
-			&query,
-			&(
-				Count,
-				tantivy::collector::TopDocs::with_limit(50)
-					.and_offset(request_query.offset)
-					.order_by_fast_field::<DateTime>("published_at", Order::Desc),
-			),
-		)
-		.unwrap();
+		let top_docs = top_docs
+			.into_iter()
+			.map(|(_, addr)| addr)
+			.collect::<Vec<_>>();
+
+		(count, top_docs)
+	} else {
+		let (count, top_docs) = searcher
+			.search(
+				&app_state.query_parser.parse_query(query)?,
+				&(
+					Count,
+					tantivy::collector::TopDocs::with_limit(50).and_offset(request_query.offset),
+				),
+			)
+			.unwrap();
+
+		let mut top_docs = top_docs
+			.into_iter()
+			.map(|(score, addr)| {
+				let segment_reader = searcher.segment_reader(addr.segment_ord);
+				let fast_field_reader = segment_reader.fast_fields().date("published_at").unwrap();
+				let published_at = fast_field_reader.first(addr.doc_id).unwrap();
+
+				(score, published_at, addr)
+			})
+			.collect::<Vec<_>>();
+
+		top_docs.sort_by(|a, b| {
+			b.0.partial_cmp(&a.0)
+				.unwrap_or(std::cmp::Ordering::Equal)
+				.then_with(|| b.1.cmp(&a.1))
+		});
+
+		let top_docs = top_docs
+			.into_iter()
+			.map(|(_, _, addr)| addr)
+			.collect::<Vec<_>>();
+
+		(count, top_docs)
+	};
 
 	let source = Arc::new(app_state.source.clone().read_owned().await);
 
@@ -58,18 +95,18 @@ pub async fn search_packages(
 	let mut tasks = top_docs
 		.into_iter()
 		.enumerate()
-		.map(|(i, (_, doc_address))| {
-			let app_state = app_state.clone();
+		.map(|(i, doc_address)| {
 			let doc = searcher.doc::<HashMap<_, _>>(doc_address).unwrap();
+			let id = (&doc[&id_field])
+				.as_str()
+				.unwrap()
+				.parse::<PackageName>()
+				.unwrap();
+
+			let app_state = app_state.clone();
 			let source = source.clone();
 
 			async move {
-				let id = (&doc[&id])
-					.as_str()
-					.unwrap()
-					.parse::<PackageName>()
-					.unwrap();
-
 				let file = read_package(&app_state, &id, &source).await?.unwrap();
 
 				let (version_id, _) = find_max_searchable(&file).unwrap();
