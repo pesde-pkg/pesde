f4e5077d94df79de8a66110e68784f7b4fa9da22
daimond113 <contact@daimond113.com>
2025-12-17 20:58:36 +0100

    feat: switch sourcemap_generator to croshet

--- src/source/wally/compat_util.rs
+++ src/source/wally/compat_util.rs
@@ -1,4 +1,7 @@
-use std::path::Path;
+use std::{
+	io::{BufRead as _, BufReader, PipeReader, Read as _},
+	path::Path,
+};
 
 use relative_path::RelativePathBuf;
 use serde::Deserialize;
@@ -6,7 +9,7 @@ use serde::Deserialize;
 use crate::{
 	LINK_LIB_NO_FILE_FOUND, Project,
 	manifest::target::Target,
-	scripts::{ExecuteScriptHooks, ScriptName, execute_script},
+	scripts::{ExecuteScriptHooks, SOURCEMAP_GENERATOR, execute_script},
 	source::{
 		traits::GetTargetOptions,
 		wally::manifest::{Realm, WallyManifest},
@@ -22,12 +25,65 @@ struct SourcemapNode {
 	file_paths: Vec<RelativePathBuf>,
 }
 
-#[derive(Debug, Clone, Copy)]
-struct CompatExecuteScriptHooks;
+#[derive(Debug, Default)]
+pub struct SourcemapGeneratorHooks {
+	stdout: String,
+	stdout_reader: Option<PipeReader>,
+	stderr_reader: Option<PipeReader>,
+}
 
-impl ExecuteScriptHooks for CompatExecuteScriptHooks {
-	fn not_found(&self, script: ScriptName) {
-		tracing::warn!("no {script} found in project. wally types will not be generated");
+impl ExecuteScriptHooks for SourcemapGeneratorHooks {
+	type Error = std::io::Error;
+
+	fn stdio(
+		&mut self,
+	) -> (
+		croshet::ShellPipeWriter,
+		croshet::ShellPipeWriter,
+		croshet::ShellPipeReader,
+	) {
+		let (stdout_reader, stdout_writer) = std::io::pipe().unwrap();
+		self.stdout_reader = Some(stdout_reader);
+		let (stderr_reader, stderr_writer) = std::io::pipe().unwrap();
+		self.stderr_reader = Some(stderr_reader);
+		let stdin_reader = std::io::pipe().unwrap().0;
+
+		(
+			croshet::ShellPipeWriter::OsPipe(stdout_writer),
+			croshet::ShellPipeWriter::OsPipe(stderr_writer),
+			croshet::ShellPipeReader::OsPipe(stdin_reader),
+		)
+	}
+
+	async fn run(&mut self) -> Result<(), Self::Error> {
+		let mut stdout_reader = self.stdout_reader.take().unwrap();
+		let stderr_reader = self.stderr_reader.take().unwrap();
+
+		let (stdout, stderr) = tokio::join!(
+			tokio::task::spawn_blocking(move || {
+				let mut string = String::new();
+				stdout_reader.read_to_string(&mut string).map(|_| string)
+			}),
+			tokio::task::spawn_blocking(move || {
+				let stderr_reader = BufReader::new(stderr_reader);
+				for line in stderr_reader.lines() {
+					match line {
+						Ok(line) => {
+							tracing::error!("[{SOURCEMAP_GENERATOR}]: {line}");
+						}
+						Err(e) => {
+							tracing::error!(
+								"ERROR IN READING STDERR OF {SOURCEMAP_GENERATOR}: {e}"
+							);
+						}
+					}
+				}
+			})
+		);
+		self.stdout = stdout.unwrap()?;
+		stderr.unwrap();
+
+		Ok(())
 	}
 }
 
@@ -35,19 +91,22 @@ async fn find_lib_path(
 	project: &Project,
 	package_dir: &Path,
 ) -> Result<Option<RelativePathBuf>, errors::GetTargetError> {
-	let Some(result) = execute_script(
-		ScriptName::SourcemapGenerator,
+	let mut hooks = SourcemapGeneratorHooks::default();
+	let ran = execute_script(
+		SOURCEMAP_GENERATOR,
 		project,
-		CompatExecuteScriptHooks,
+		&mut hooks,
 		vec![package_dir.into()],
-		true,
 	)
-	.await?
-	.filter(|result| !result.is_empty()) else {
+	.await?;
+	if !ran {
+		tracing::warn!(
+			"no `{SOURCEMAP_GENERATOR}` found in project. wally types will not be generated"
+		);
 		return Ok(None);
-	};
+	}
 
-	let node: SourcemapNode = serde_json::from_str(&result)?;
+	let node: SourcemapNode = serde_json::from_str(&hooks.stdout)?;
 	Ok(node.file_paths.into_iter().find(|path| {
 		path.extension()
 			.is_some_and(|ext| ext == "lua" || ext == "luau")
@@ -80,6 +139,8 @@ pub(crate) async fn get_target(
 pub mod errors {
 	use thiserror::Error;
 
+	use super::SourcemapGeneratorHooks;
+
 	/// Errors that can occur when finding the lib path
 	#[derive(Debug, Error)]
 	#[non_exhaustive]
@@ -90,7 +151,7 @@ pub mod errors {
 
 		/// An error occurred while executing a script
 		#[error("error executing script")]
-		ExecuteScript(#[from] crate::scripts::errors::ExecuteScriptError),
+		ExecuteScript(#[from] crate::scripts::errors::ExecuteScriptError<SourcemapGeneratorHooks>),
 
 		/// An error occurred while deserializing the sourcemap result
 		#[error("error deserializing sourcemap result")]
