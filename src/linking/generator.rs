90c3af00820d8392781476453b37246b90a75f6a
EwDev <74792428+ewd3v@users.noreply.github.com>
2026-03-18 19:05:14 +0100

    feat: re-export type functions in dependency linkers (#68)
    * re-export type functions
    
    * add entry to changelog
    
    * refactor: use chains instead of iterator
    
    * fix: ignore type functions without parameters
    
    check comment in commit
    
    * Revert "fix: ignore type functions without parameters"
    
    This reverts commit 3b2e85209c2bf5fba36d32c20eb08094b7a5a580.
    Even if it does result in a type error, it will not hurt the other
    exports, which means it's safe to export. This seems to be a bug with
    Luau anyways.
    
    * fix: remove empty generics on left hand side
    
    * fix: changelog entry labeled as a feature
    
    * fix: skip re-exporting type functions without parameters
    
    * refactor: fix lints
    
    ---------
    
    Co-authored-by: daimond113 <contact@daimond113.com>

--- src/linking/generator.rs
+++ src/linking/generator.rs
@@ -1,7 +1,10 @@
 use std::path::{Component, Path};
 
 use crate::manifest::{target::TargetKind, Manifest};
-use full_moon::{ast::luau::ExportedTypeDeclaration, visitors::Visitor};
+use full_moon::{
+	ast::luau::{ExportedTypeDeclaration, ExportedTypeFunction},
+	visitors::Visitor,
+};
 use relative_path::RelativePath;
 use tracing::instrument;
 
@@ -38,6 +41,27 @@ impl Visitor for TypeVisitor {
 
 		self.types.push(format!(
 			"export type {name}{declaration_generics} = module.{name}{generics}\n"
+		));
+	}
+
+	fn visit_exported_type_function(&mut self, node: &ExportedTypeFunction) {
+		let name = node.type_function().function_name().to_string();
+		let params = node
+			.type_function()
+			.function_body()
+			.parameters()
+			.into_iter()
+			.map(ToString::to_string)
+			.collect::<Vec<String>>();
+
+		// Not possible to re-export type functions without parameters as a type declaration
+		if params.is_empty() {
+			return;
+		}
+
+		let generics = format!("<{}>", params.join(", "));
+		self.types.push(format!(
+			"export type {name}{generics} = module.{name}{generics}\n"
 		));
 	}
 }
