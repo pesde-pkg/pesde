c9cd049c733e7c5b2135844c1b99b498565036ec
daimond113 <contact@daimond113.com>
2026-06-01 20:45:53 +0200

    refactor: abandon seqs completely

--- registry/src/features/log/get_head.rs
+++ registry/src/features/log/get_head.rs
@@ -31,45 +31,17 @@ async fn handler(
 	db: &Database,
 	size_from: Option<NonZeroU64>,
 ) -> AppResult<Option<LogHeadResponse>> {
-	let Some((seq, mmr_size)) = query(db).await? else {
-		return Ok(None);
-	};
-
-	let mmr = db.read_mmr_sized(mmr_size);
+	let mmr = db.read_mmr().await?;
 
 	Ok(Some(LogHeadResponse {
-		seq,
 		accumulator: mmr.get_accumulator().await?,
 		state: match size_from {
 			Some(size_from) => LogHeadResponseState::WithPreviousState {
 				proof: mmr.gen_consistency_proof(size_from.get()).await?,
 			},
 			None => LogHeadResponseState::OnlyNewState {
-				mmr_size_to: mmr_size,
+				mmr_size_to: mmr.mmr_size(),
 			},
 		},
 	}))
-}
-
-async fn query(db: &Database) -> anyhow::Result<Option<(EntrySeq, u64)>> {
-	match db {
-		Database::MySql(pool) => {
-			let Some(record) = sqlx::query!(
-				r#"
-				SELECT seq, COUNT(TreeNode.pos) AS `mmr_size: u64`
-				FROM TreeNode
-				INNER JOIN LogEntry ON LogEntry.pos=TreeNode.pos
-				ORDER BY seq DESC
-				LIMIT 1
-				"#
-			)
-			.fetch_optional(pool)
-			.await?
-			else {
-				return Ok(None);
-			};
-
-			Ok(record.seq.map(|seq| (EntrySeq(seq), record.mmr_size)))
-		}
-	}
 }
