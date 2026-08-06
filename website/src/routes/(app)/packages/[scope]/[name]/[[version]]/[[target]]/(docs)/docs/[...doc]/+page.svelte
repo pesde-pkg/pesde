828a3c5c14385f3c6dbf9f0bebd88fb4ad0e3d6a
daimond113 <contact@daimond113.com>
2026-08-06 20:55:31 +0200

    chore: delete website

--- website/src/routes/(app)/packages/[scope]/[name]/[[version]]/[[target]]/(docs)/docs/[...doc]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	const { data } = $props()
-</script>
-
-<main class="prose max-w-none py-16">
-	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	{@html data.html}
-</main>
